62242d3b2100a39f2ad76a334cb3dec4c9f26938
mpkbt <mpkbt@pop-os.localdomain>
2021-12-06 21:54:38 -0300

    randomness cap

--- index.py
+++ index.py
@@ -66,21 +66,22 @@ last_log_is_progress = False
 
 
 
-def addRandomness(n):
-    if n < 50:
-        print("INT TO LOW FOR RANDOMNESS")
-    randomness_percentage = 0
-    random_factor = 2 * random() * randomness_percentage * n
+def addRandomness(n, randomn_factor_size=None):
+    if randomn_factor_size is None:
+        randomness_percentage = 0.1
+        randomn_factor_size = randomness_percentage * n
+
+    random_factor = 2 * random() * randomn_factor_size
     if random_factor > 5:
         print( '!!! -----------too big' )
         random_factor = 5
-    without_average_random_factor = n - (randomness_percentage * n)
+    without_average_random_factor = n - randomn_factor_size
     randomized_n = int(without_average_random_factor + random_factor)
-    logger('{} with randomness -> {}'.format(n, randomized_n))
+    logger('{} with randomness -> {}'.format(int(n), randomized_n))
     return int(randomized_n)
 
 def moveToWithRandomness(x,y,t):
-    pyautogui.moveTo(addRandomness(x),addRandomness(y),t+random()/2)
+    pyautogui.moveTo(addRandomness(x,10),addRandomness(y,10),t+random()/2)
 
 
 def remove_suffix(input_string, suffix):
