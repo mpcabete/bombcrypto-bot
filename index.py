9c530b144ce16adafdb434c257ee66a8bb381a79
Thiago Oliveira <thiago.zeyu@gmail.com>
2021-12-09 22:32:50 -0300

    Update index.py

--- index.py
+++ index.py
@@ -39,15 +39,6 @@ def telegram_bot_sendphoto(photo_path):
     global bot
     return bot.send_photo(chat_id=TELEGRAM_CHAT_ID, photo=open(photo_path, 'rb'))
 
-
-# def telegram_bot_sendtext(bot_message):
-#     bot_token = '5065947731:AAGsr0Uov3Qe0LeQlF1jPD-jYcVe5b0vAIo'
-#     bot_chatID = '1970213580'
-#     send_text = 'https://api.telegram.org/bot' + bot_token + '/sendMessage?chat_id=' + bot_chatID + '&parse_mode=Markdown&text=' + bot_message
-#     response = requests.get(send_text)
-#     return response.json()
-# 
-
 cat = """
                                                 _
                                                 \`*-.
