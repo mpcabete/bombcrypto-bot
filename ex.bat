b421b872ec0f9ff5f671ad184b0a9e5e449782eb
bnudaniel22 <97205983+bnudaniel22@users.noreply.github.com>
2022-01-15 14:15:05 -0300

    Revert "Multi Contas + Correções"
    This reverts commit fcf464324a359c91b6eaad2271983c9db5f30396.

--- ex.bat
+++
@@ -1,1 +0,0 @@
-python forever.py index.py
--- forever.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-from subprocess import Popen
-import sys
-
-filename = sys.argv[1]
-while True:
-    print("\nStarting " + filename)
-    p = Popen("python " + filename, shell=True)
-    p.wait()
